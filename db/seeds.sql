51fa90f3b776bb509752682bee9a622deb88c0a7
MugishaU <muwiragiye123@gmail.com>
2020-10-13 23:12:01 +0100

    add seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    uid varchar(28),
+    username varchar(255)
+);
+
+INSERT INTO users (uid, username)
+VALUES
+("yKyLmkyIKmTTr3W7jJay6ygRULv2","user1"),
+("sOBdjNvWYcdXV1O0niEIawWEDNu2","user2"),
+("D4Pe4XwHp0ZmSso7ek4jcu5fnYH2","user3");
