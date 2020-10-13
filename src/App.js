49761af864eb2a05fafde8c062a4e62273bd0d53
MugishaU <muwiragiye123@gmail.com>
2020-10-13 15:08:00 +0100

    add anon sign in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from "react";
+import * as firebase from "firebase/app";
+import {
+  FirebaseAuthProvider,
+  FirebaseAuthConsumer
+} from "@react-firebase/auth";
+import { config } from "../src/firebaseConfig";
+const concept = "world";
+import "firebase/auth";
 import "./styles/App.css";
+
 class App extends React.Component {
   state = {};
   render() {
@@ -7,8 +16,54 @@ class App extends React.Component {
       <>
         <h1>New React App</h1>
         <h2>Happy Coding</h2>
+
+        <FirebaseAuthProvider {...config} firebase={firebase}>
+          <div>
+            Hello <div>From Auth Provider Child 1</div>
+            <FirebaseAuthConsumer>
+               {({ isSignedIn, firebase }) => {
+              if (isSignedIn === true) {
+                return (
+                  <div>
+                    <h2>You're signed in 🎉 </h2>
+                    <button
+                      onClick={() => {
+                        firebase
+                          .app()
+                          .auth()
+                          .signOut();
+                      }}
+                    >
+                      Sign out
+                    </button>
+                  </div>
+                );
+              } else {
+                return (
+                  <div>
+                    <h2>You're not signed in </h2>
+                    <button
+                      onClick={() => {
+                        firebase
+                          .app()
+                          .auth()
+                          .signInAnonymously();
+                      }}
+                    >
+                      Sign in anonymously
+                    </button>
+                  </div>
+                );
+              }
+            }}
+            </FirebaseAuthConsumer>
+          </div>
+          <div>Another div</div>
+       </FirebaseAuthProvider>
       </>
     );
   }
 }
+
+
 export default App;
